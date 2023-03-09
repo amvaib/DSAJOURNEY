16c3338499e1a76dec4b4965cec140b95990f18f
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2023-03-09 23:07:09 +0530

    loop and string

---
+++ string.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int len;
+    string s = "vaibhav";
+    
+    len = s.size();
+    s[ len- 1] = 'z';
+    cout << s;
+
+
+    return 0;
+
+}
